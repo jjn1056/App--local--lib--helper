35080b1ec05102a4dbdd6b7735990d4b79271abb
John Napiorkowski <johnn@John-Napiorkowski-MacBook-Pro.local>
2010-08-04 18:10:05 -0400

    better POD

--- lib/App/local/lib/helper.pm
+++ lib/App/local/lib/helper.pm
@@ -100,7 +100,7 @@ END
 
 =head1 NAME
 
-App::local::lib::helper - Functionality to build a L<local::lib> helper script
+App::local::lib::helper - Make it easy to run code against a L<local::lib>
 
 =head1 SYNOPSIS
 
@@ -109,12 +109,84 @@ App::local::lib::helper - Functionality to build a L<local::lib> helper script
 
 =head1 DESCRIPTION
 
-This is an object with provide the functionality to create a L<local::lib>
-'helper' script in either the currently loaded L<local::lib> enviroment or in
-a target directory of choice.
+This is an object which provide the functionality to create a L<local::lib>
+'helper' script in either the currently loaded L<local::lib> environment or in
+a target directory of choice.  By default the script is called 'localenv' and
+can be used to invoke a command under the L<local::lib> which it was built
+against.  For example, assume you build a L<local::lib> like so:
 
-You should see the POD for the helper script as well as the script which is
-using this class.  You probably won't use this class directly.
+    cpanm -L ~/mylib App::local::lib::helper
+
+This creates a directory "~/mylib" and installs L<App::local::lib::helper> and
+a utility script into "~/mylib/bin".  Now, if you want to invoke a perl 
+application and use libs installed into "~/mylib", you can do so like:
+
+    ~/mylib/bin/locallib perl [SOME COMMAND]
+
+The command C<locallib> will make sure the same L<local:lib> that was active
+when L<App::local::lib::helper> was originally installed is again installed
+into the environment before executing the commands passed in @ARGV.  Upon
+completing the command, the %ENV is restored so that you can use this to fire
+off an application against a specific L<local::lib> without needing to deal
+with the details of how to activate the L<local::lib> or how to make sure
+your %ENV stays clean.
+
+The arguments given to C<locallib> don't need to be a perl application.  For
+example, I often like to open a sub shell under a particular L<local::lib>
+managed directory.
+
+    ~/mylib/bin/locallib bash
+
+Now, if I do:
+
+    perl -V
+
+I'll see that ~/mylib has been added to @INC.  Additionally, "~/mylib/bin" will
+have been added to $PATH, so that any command line perl applications installed
+into the L<local::lib> (such as C<ack> or C<cpanm>) can be accessed easily.
+
+Another example usage would be when you want to install an application from
+CPAN, install it and all its dependencies to a single directory root and 
+then run it without a lot of effort.  For example:
+
+    cpanm -L ~/gitalyst-libs Gitalist App::local::lib::helper
+    ~/gitalyst-libs/bin/localenv gitalyst-server.pl
+
+And presto! Your cpan installed application is running, fully self-contained to
+one root directory all under regular user privileges.
+
+L<local::lib> does all the real work, but I find this to be the easiest way to
+run given code against a L<local::lib> root.  
+
+=head1 OPTIONS
+
+This class supports the following options.
+
+=over 4
+
+=item which_perl
+
+This should be the path to the perl binary that the L<local::lib> is built
+against. This defaults to the path of the perl binary under which we are
+currently running.  You should probably leave this one alone :)
+
+=item target
+
+This is the target directory for the L<local::lib> you want to build the helper
+script against.  By default it will attempt to detect the currently running
+L<local::lib> and use that.  If we can't detect a running L<local::lib> and
+this option is undef, we die with a message.
+
+=item helper_name
+
+This is the name of the helper utility script.  It defaults to 'localenv'.
+
+=item helper_permissions
+
+These are the permissions the the helper utility script is set to.  By default
+we set the equivilent of 'chmod 755 [HELPER SCRIPT]'
+
+=back
 
 =head1 AUTHOR
 
